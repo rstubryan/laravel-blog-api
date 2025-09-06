9a63aec5236f1e5532f3d47f2162b9335dc2ec01
rstubryan <restubumirr@gmail.com>
2025-09-06 22:26:49 +0700

    refactor: remove unused Str import from CategoryController

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -5,7 +5,6 @@ namespace App\Http\Controllers;
 use App\Models\Category;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
-use Illuminate\Support\Str;
 use Illuminate\Validation\ValidationException;
 
 class CategoryController extends Controller
